47f7f0a6f5ee50b0fbdbee700280a25441c3eda7
Rob Phillips <rob@getkeepsafe.com>
2018-04-26 12:23:38 -0400

    [Project] Initial project skeleton

---
+++ TracerTests/TracerTests.swift
@@ -0,0 +1,15 @@
+//
+//  TracerTests.swift
+//  TracerTests
+//
+//  Created by Rob Phillips on 4/26/18.
+//  Copyright © 2018 Keepsafe Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Tracer
+
+class TracerTests: XCTestCase {
+    
+    
+}
